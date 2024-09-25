356c113510ba4e86e964092a8127b33ceefd0668
HTLA380 <htetlin099798@gmail.com>
2024-09-25 17:53:09 +0630

    feat: add page progress loader

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import NextTopLoader from "nextjs-toploader";
 import QueryProvider from "~/components/providers/QueryProvider";
 import ThemeProvider from "~/components/providers/ThemeProvider";
 import { Toaster } from "~/components/toaster/Toaster";
@@ -24,6 +25,11 @@ export default function RootLayout({
         <QueryProvider>
           <ThemeProvider>
             <Toaster />
+            <NextTopLoader
+              color="#f97316"
+              shadow="0 0 2px #f97316,0 0 3px #f97316"
+              showSpinner={false}
+            />
             {children}
           </ThemeProvider>
         </QueryProvider>
